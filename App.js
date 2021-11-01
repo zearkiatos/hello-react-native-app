769517ff8ede49fc84c532d9ff1a493f4226d6de
zearkiatos <caprilespe@outlook.com>
2021-11-01 13:32:31 -0300

    More buttons

--- App.js
+++ App.js
@@ -7,6 +7,10 @@ import {
   TextInput,
   Dimensions,
   Button,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
 } from "react-native";
 import Greeting from "./src/components/Greeting";
 
@@ -15,6 +19,10 @@ const width = Dimensions.get("window").width;
 export default function App() {
   const [text, setText] = useState("Happy 🐷");
   const [submit, setSubmit] = useState("");
+  const onPress = () => {
+    setSubmit(text);
+    alert("Text sended successful");
+  };
   return (
     <View style={styles.container}>
       <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
@@ -26,18 +34,50 @@ export default function App() {
         onChangeText={(t) => setText(t)}
         defaultValue={text}
       />
-      <Button onPress={() => {
-        setSubmit(text);
-        alert('Text sended successful');
-      }} title="Accept" />
+      <Button onPress={onPress} title="Accept" />
+      <TouchableHighlight
+        underlayColor={"#999"}
+        activeOpacity={0.2}
+        onPress={onPress}
+      >
+        <Text>Accept</Text>
+      </TouchableHighlight>
+      <TouchableNativeFeedback
+        background={TouchableNativeFeedback.Ripple("#00f", true)}
+        onPress={onPress}
+      >
+        <View style={styles.view}>
+          <Text>Accept</Text>
+        </View>
+      </TouchableNativeFeedback>
+      <TouchableOpacity style={styles.touchableOpacity} onPress={onPress}>
+        <View style={styles.view}>
+          <Text>Accept</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableWithoutFeedback
+        onPress={onPress}
+      >
+        <View style={styles.view}>
+          <Text>Accept</Text>
+        </View>
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  touchableOpacity: {
+    backgroundColor: "#eee",
+  },
+  view: {
+    height: 40,
+    width: 300,
+  },
   input: {
     height: 40,
+    alignItems: "center",
     borderBottomColor: "#ccc",
     borderBottomWidth: 1,
     width: "100%",
