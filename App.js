cd2220a75c40242e3f2d16e5b3605ccc14015a49
Pedro Capriles M <p0c04cw@M-C02Z9029LVCG.local>
2021-11-03 08:41:51 -0300

    List in react native and flatList

--- App.js
+++ App.js
@@ -1,148 +1,56 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  Dimensions,
-  Button,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  ScrollView,
-} from "react-native";
-import Greeting from "./src/components/Greeting";
-
-const width = Dimensions.get("window").width;
+import React from "react";
+import { StyleSheet, View, FlatList, Text } from "react-native";
 
 export default function App() {
-  const [text, setText] = useState("Happy 🐷");
-  const [submit, setSubmit] = useState("");
-  const onPress = () => {
-    setSubmit(text);
-    alert("Text sended successful");
-  };
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Greeting text="Hello World! 👋 🌎" style={[styles.text, styles.red]} />
-        <Text>Text: {text}</Text>
-        <Text>Data Submitted: {submit}</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Write Here"
-          onChangeText={(t) => setText(t)}
-          defaultValue={text}
-        />
-        <Button onPress={onPress} title="Accept" />
-        <TouchableHighlight
-          underlayColor={"#999"}
-          activeOpacity={0.2}
-          onPress={onPress}
-        >
-          <Text>Accept</Text>
-        </TouchableHighlight>
-        <TouchableNativeFeedback
-          background={TouchableNativeFeedback.Ripple("#00f", true)}
-          onPress={onPress}
-        >
-          <View style={styles.view}>
-            <Text>Accept</Text>
-          </View>
-        </TouchableNativeFeedback>
-        <TouchableOpacity style={styles.touchableOpacity} onPress={onPress}>
-          <View style={styles.view}>
-            <Text>Accept</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableWithoutFeedback onPress={onPress}>
-          <View style={styles.view}>
-            <Text>Accept</Text>
-          </View>
-        </TouchableWithoutFeedback>
-        <StatusBar style="auto" />
-      </ScrollView>
+      <FlatList
+        data={[
+          { key: "1", name: "Nicolas" },
+          { key: "2", name: "Pedro" },
+          { key: "3", name: "Juan" },
+          { key: "4", name: "Maria" },
+          { key: "5", name: "Josefa" },
+          { key: "6", name: "Nicolas" },
+          { key: "7", name: "Pedro" },
+          { key: "8", name: "Juan" },
+          { key: "9", name: "Maria" },
+          { key: "10", name: "Josefa" },
+          { key: "11", name: "Nicolas" },
+          { key: "12", name: "Pedro" },
+          { key: "13", name: "Juan" },
+          { key: "14", name: "Maria" },
+          { key: "15", name: "Josefa" },
+          { key: "16", name: "Nicolas" },
+          { key: "17", name: "Pedro" },
+          { key: "18", name: "Juan" },
+          { key: "19", name: "Maria" },
+          { key: "20", name: "Josefa" },
+          { key: "21", name: "Nicolas" },
+          { key: "22", name: "Pedro" },
+          { key: "23", name: "Juan" },
+          { key: "24", name: "Maria" },
+          { key: "25", name: "Josefa" },
+        ]}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      ></FlatList>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  touchableOpacity: {
-    backgroundColor: "#eee",
-  },
-  view: {
-    height: 40,
-    width: 300,
-  },
-  input: {
-    height: 40,
-    alignItems: "center",
-    borderBottomColor: "#ccc",
-    borderBottomWidth: 1,
-    width: "100%",
-  },
-  text: {
-    fontSize: 24,
-    color: "white",
-  },
-  red: {
-    backgroundColor: "red",
-  },
-  green: {
-    backgroundColor: "green",
-  },
-  blue: {
-    backgroundColor: "blue",
-  },
   container: {
     flex: 1,
-    flexDirection: "column",
     backgroundColor: "#fff",
-    alignItems: "center",
+    alignItems: "stretch",
     justifyContent: "center",
+    paddingTop: 22,
   },
-  scrollView: {
-    width
+  item: {
+    padding: 10,
+    fontSize: 22,
+    height: 50,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
   }
 });
