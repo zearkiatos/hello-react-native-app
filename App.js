1ea59441937d84f3d59cc78209d53b6430b7cba7
Pedro Capriles M <p0c04cw@M-C02Z9029LVCG.local>
2021-11-05 08:24:44 -0300

    SectionList

--- App.js
+++ App.js
@@ -1,39 +1,25 @@
 import React from "react";
-import { StyleSheet, View, FlatList, Text } from "react-native";
+import { StyleSheet, View, FlatList, Text, SectionList } from "react-native";
+import data from "./src/mock/data.json";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <FlatList
-        data={[
-          { key: "1", name: "Nicolas" },
-          { key: "2", name: "Pedro" },
-          { key: "3", name: "Juan" },
-          { key: "4", name: "Maria" },
-          { key: "5", name: "Josefa" },
-          { key: "6", name: "Nicolas" },
-          { key: "7", name: "Pedro" },
-          { key: "8", name: "Juan" },
-          { key: "9", name: "Maria" },
-          { key: "10", name: "Josefa" },
-          { key: "11", name: "Nicolas" },
-          { key: "12", name: "Pedro" },
-          { key: "13", name: "Juan" },
-          { key: "14", name: "Maria" },
-          { key: "15", name: "Josefa" },
-          { key: "16", name: "Nicolas" },
-          { key: "17", name: "Pedro" },
-          { key: "18", name: "Juan" },
-          { key: "19", name: "Maria" },
-          { key: "20", name: "Josefa" },
-          { key: "21", name: "Nicolas" },
-          { key: "22", name: "Pedro" },
-          { key: "23", name: "Juan" },
-          { key: "24", name: "Maria" },
-          { key: "25", name: "Josefa" },
-        ]}
+        data={data}
         renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
       ></FlatList>
+      <SectionList
+        sections={[
+          { title: "Group 1", data: data.slice(0, 4) },
+          { title: "Group 2", data: data.slice(5, 10) },
+          { title: "Group 3", data: data.slice(11, 15) },
+        ]}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+        renderSectionHeader={({ section }) => (
+          <Text style={styles.section}>{section.title}</Text>
+        )}
+      />
     </View>
   );
 }
@@ -50,7 +36,16 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 22,
     height: 50,
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  section: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    backgroundColor: '#ccc',
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2
   }
 });
