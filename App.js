0c8e3960410e007b3012665a32d4bbf8d9328b8f
Pedro Capriles <caprilespe@outlook.com>
2021-11-06 12:26:11 -0300

    Maps

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import ModalDemo from "./src/components/ModalDemo";
-import AlertDemo from "./src/components/AlertDemo";
 import GeolocalizationDemo from "./src/components/GeolocalizationDemo";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ModalDemo />
-      <AlertDemo />
       <GeolocalizationDemo />
     </View>
   );
--- src/components/GeolocalizationDemo/index.js
+++ src/components/GeolocalizationDemo/index.js
@@ -1,25 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, Alert } from 'react-native';
 import * as Location from 'expo-location';
 import Constants from 'expo-constants';
+import MapView from 'react-native-maps';
+import { Dimensions } from 'react-native-web';
 const PERMISSIONS = {
     GRANTED: 'granted'
-}
+};
+
 const GeolocalizationDemo = () => {
+    const [location, setLocation] = useState(null);
     const searchLocation = async () => {
-        const { status } = await Location.requestPermissionsAsync();
+        const { status } = await Location.requestForegroundPermissionsAsync();
         if (status !== PERMISSIONS.GRANTED) {
+            setErrorMessage
             return Alert.alert('We do not have the necessary permissions to access to the location');
         };
         const location = await Location.getCurrentPositionAsync({});
-        console.log(location);
+        console.log('Location:', location);
+        setLocation(location)
     };
 
     useEffect(() => {
         searchLocation()
-    });
+    }, []);
+    let text = 'Waiting..';
+    if (!location) 
+        return <Text>{text}</Text>
+  
+    console.log(location);
     return (
         <View style={styles.container}>
+            <MapView style={styles.map} />
         </View>
     )
 };
@@ -32,6 +44,10 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         paddingTop: 22,
     },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    }
 });
 
 
